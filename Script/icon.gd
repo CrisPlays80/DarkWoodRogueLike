5fd44c151b976690c1d67beaaaa986641f02eb47
CrisPlays80 <cristian.steven05@gmail.com>
2024-09-10 07:43:54 -0500

    Implementando el dash

--- Script/icon.gd
+++ Script/icon.gd
@@ -1,7 +1,10 @@
 extends CharacterBody2D
 
-var SPEED = 500
+var SPEED = 200
+var dash_speed = 10
 
 func _physics_process(delta: float) -> void:
 	velocity = Input.get_vector("Izquierda", "Derecha", "Arriba", "Abajo") * SPEED
+	if Input.is_action_just_pressed("Dash"):
+		velocity *= dash_speed
 	move_and_slide()
